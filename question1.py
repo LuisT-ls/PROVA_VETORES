ed9784111cfa8b6e2ff2fd99ecfb32c9e96332c5
LuisT-ls <luishg213@outlook.com>
2022-05-22 01:48:28 -0300

    commit2

--- question1.py
+++ question1.py
@@ -3,9 +3,10 @@ arraysnames = []
 count = 0
 i = []
 
-for count in range(4):
+for count in range(10):
     username = str(input("\n"))
     arraysnames.append(username)
 
-    i = arraysnames.index(username)
-    print(f"Nome = {username}", "\n",  "Posição =", i)
+for element in enumerate(arraysnames):
+    print(element)
+    
