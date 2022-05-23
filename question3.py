5faf8d6abaa3279d550dec8b07e4f192e8026b80
LuisT_ls <luishg213@outlook.com>
2022-05-23 13:25:48 -0300

    commit7

--- question3.py
+++ question3.py
@@ -6,17 +6,21 @@ count = 0
 
 print("\nDigite o gabarito da questão  ")
 
-for count in range(5):
+for count in range(20):
     count = + 1
     gabarito = str(input())
     G.append(gabarito)
 
-for count in range(5):
+for count in range(50):
     count = + 1
     
     print("\nDigite a resposta do aluno ")
     
-    for i in range(5):
+    for i in range(20):
         respostas = str(input())
         R.append(respostas)
         
+    if(set(R) == set(G)):
+        print("\nAPROVADO")
+    else:
+        print("\nREPROVADO")
